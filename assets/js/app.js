2ebe1d36b9d748b6e69adfac75064ab6ff301084
Asrafuzzaman <asrfuzzaman2379@gmail.com>
2024-04-16 08:57:30 +0600

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,7 +57,7 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 var app = document.getElementById("autoWrite");
 
 var typewriter = new Typewriter(app, {
-  strings: ["Student", "Laravel Developer", "Python Developer", "Photographer","Traveler"],
+  strings: ["Student", "Software Developer", "Photographer", "Traveler"],
   loop: true,
   autoStart: true,
 });
